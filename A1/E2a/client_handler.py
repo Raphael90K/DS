5983ce638ea7776874decc68d203b0091c1a41e2
Raphael <raphael.kropp@web.de>
2024-06-01 12:11:57 +0200

    logs

--- A1/E2a/client_handler.py
+++ A1/E2a/client_handler.py
@@ -2,7 +2,7 @@ from tcp_client import Client
 
 clients = []
 for i in range(10):
-    client = Client("79.214.184.93", 42424, 0)
+    client = Client("127.0.0.1", 42424, 3)
     client.start_client(i)
     clients.append(client)
 
